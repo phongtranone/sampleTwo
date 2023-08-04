f48daed8cf8d121def604b4a7081b63cadc53087
tranthanhphong <phongtranone@gmail.com>
2023-08-04 18:01:27 +0700

    C1

--- src/test/java/TC001_Booking_Taxi.java
+++ src/test/java/TC001_Booking_Taxi.java
@@ -4,6 +4,7 @@ public class TC001_Booking_Taxi {
         int yourPhone;
         String yourOTP;
         String yourNotes = "Pick me at the entrance";
+        String commitStr = "C1";
     }
 
     public void addPromoCode(String promoCode){
