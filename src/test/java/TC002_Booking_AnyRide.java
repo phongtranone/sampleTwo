02a2cdfe75053a21381966808afcd0cbbaa78e66
tranthanhphong <phongtranone@gmail.com>
2023-08-04 18:03:06 +0700

    C2

--- src/test/java/TC002_Booking_AnyRide.java
+++ src/test/java/TC002_Booking_AnyRide.java
@@ -2,6 +2,7 @@ public class TC002_Booking_AnyRide {
   public void clickOnTheBooking(){
     String labelBtn = "Booking";
     String tipLabel = "Tips for driver";
+    String commitStr = "C2";
   }
 
   public void addTipsForDriver(){
